944ba61d5b32e95d227b8f3d80969b07481de88d
Jerald Babor <jeraldbabor60@gmail.com>
2026-05-20 15:51:50 +0800

    new updated about us

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -205,7 +205,7 @@ function SentosaHeading() {
 
 function TransformationSection() {
   return (
-    <section className="bg-[#151515] px-6 py-10 sm:px-10 sm:py-12 lg:px-20 lg:py-14">
+    <section className="bg-[#151515] px-6 py-12 sm:px-10 sm:py-14 lg:px-20 lg:py-16">
       <div className="pointer-events-none absolute top-1/2 left-[-400px]
          z-0 h-[520px] w-[520px]
          -translate-y-1/2 rounded-full
@@ -214,9 +214,15 @@ function TransformationSection() {
         <div className="grid gap-8 lg:grid-cols-[680px_680px] lg:items-baseline lg:justify-between lg:gap-0">
           <TransformationHeading />
           <p className="font-display font-light text-center sm:text-center lg:text-left text-[18px] leading-[1.35] tracking-[-0.04em] text-white/60">
-            Today, the leadership reigns have been passed to our current CEO, <b className="text-[#ffffffd9]">Mr. Keith Tan</b>, who
-            bring relationships from the South Australian winemaking industry. Under Keith&apos;s
-            leadership, Wine Network has pivoted to empowering host restaurants through:
+            <span className="lg:block lg:whitespace-nowrap">
+              Today, the leadership reigns have been passed to our current CEO, <b className="text-[#ffffffd9]">Mr. Keith Tan</b>, who
+            </span>{" "}
+            <span className="lg:block lg:whitespace-nowrap">
+              bring relationships from the South Australian winemaking industry. Under Keith&apos;s
+            </span>{" "}
+            <span className="lg:block lg:whitespace-nowrap">
+              leadership, Wine Network has pivoted to empowering host restaurants through:
+            </span>
           </p>
         </div>
         <div className="mt-[70px] grid gap-3 lg:grid-cols-2">
